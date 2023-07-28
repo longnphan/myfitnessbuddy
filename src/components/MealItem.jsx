a9f179bf7823e5707403e48fc52e860bf662a974
Long Phan <longnphan@outlook.com>
2023-07-28 11:37:15 -0600

    add useSelector to MealItem

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,9 @@
+import { useSelector } from "react-redux";
+
 function MealItem({ meal }) {
+  const lowercaseMeal = meal.toLowerCase();
+  const mealData = useSelector(state => state.user[lowercaseMeal]);
+
   return (
     <>
       <div className="rounded-none border border-slate-600 bg-slate-800 mt-6">
@@ -6,9 +11,15 @@ function MealItem({ meal }) {
           {meal}
         </div>
       </div>
-      <div className="rounded-none border border-slate-600 bg-slate-800">
-        <div className="collapse-title text-xl font-medium">Food Items...</div>
-      </div>
+
+      {mealData.length > 0 && (
+        <div className="rounded-none border border-slate-600 bg-slate-800">
+          <div className="collapse-title text-xl font-medium">
+            Food Items...
+          </div>
+        </div>
+      )}
+
       <div className="rounded-none border border-slate-600 bg-slate-800">
         <div className="collapse-title text-xl text-blue-500 font-medium cursor-pointer">
           ADD FOOD
