656f7c996d7f6743ac855b4ef05c642ba74ba924
Long Phan <longnphan@outlook.com>
2023-07-30 15:54:15 -0600

    rm unused action creators in userSlice

--- src/store/userSlice.jsx
+++ src/store/userSlice.jsx
@@ -8,15 +8,10 @@ const userSlice = createSlice({
     lunch: [],
     dinner: [],
     snacks: [],
-    searchFood: [],
   },
   reducers: {
     updateProfile(state, action) {
       state.profile = action.payload;
-    },
-    updateSearchFood(state, action) {
-      state.searchFood = action.payload;
-      console.log("data in foodData Slice:", state.searchFood);
     },
     addFood(state, action) {
       switch (action.payload.meal) {
@@ -35,17 +30,6 @@ const userSlice = createSlice({
         default:
           return;
       }
-
-      const mealType = action.payload.meal;
-      `state.${mealType}`.push(action.payload.food);
-      // console.log("meal type is:", mealType);
-      // console.log("payload is:", action.payload.food);
-      // state.mealType.push(action.payload);
-      console.log(`Food items in ${state[action.meal]}`, state[action.meal]);
-      console.log("breakfast arr", state.breakfast);
-      console.log("lunch arr", state.lunch);
-      console.log("dinner arr", state.dinner);
-      console.log("snacks arr", state.snacks);
     },
   },
 });
