cf7b503571c2f0fc039bd83996ac589e0fe65f4f
Long Phan <longnphan@outlook.com>
2023-07-30 12:30:00 -0600

    refactored searchFoods get request and updated SearchFoods component

--- src/components/AddFoodItem.jsx
+++ src/components/AddFoodItem.jsx
@@ -1,13 +1,17 @@
 function AddFoodItem({ foodItem }) {
-  const { brand, knownAs } = foodItem.food;
+  const { label } = foodItem.food;
   const { ENERC_KCAL, CHOCDF, PROCNT, FAT } = foodItem.food.nutrients;
 
   return (
-    <div className="rounded-none border border-slate-600 bg-slate-800 mt-4">
+    <div className="rounded-none border border-slate-600 bg-slate-800 mt-4 h-20">
       <div className="collapse-title text-l font-bold font-medium">
-        <p>
-          {brand}: {knownAs} , Calories: {ENERC_KCAL}, Protein: {PROCNT},
-          Carbohydrates: {CHOCDF}, Fat: {FAT}
+        <p>{label}</p>
+        <p className="flex justify-between">
+          Calories: {Math.round(ENERC_KCAL)} | Protein: {Math.round(PROCNT)} |
+          Carbohydrates: {Math.round(CHOCDF)} | Fat: {Math.round(FAT)}{" "}
+          <button className="btn btn-ghost border-none normal-case text-2xl cursor-pointer hover:text-slate-400 hover:bg-transparent pb-4">
+            +
+          </button>
         </p>
       </div>
     </div>
--- src/utils/API.jsx
+++ src/utils/API.jsx
@@ -4,12 +4,15 @@ const apiId = import.meta.env.VITE_API_ID;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export const searchFoods = async query => {
-  const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${apiId}&app_key=${apiKey}&ingr=${query}`;
-  const response = await axios.get(url);
+  const url = "https://api.edamam.com/api/food-database/v2/parser";
+
+  const response = await axios.get(url, {
+    params: {
+      app_id: apiId,
+      app_key: apiKey,
+      ingr: query,
+    },
+  });
 
   return response.data.hints;
-};
-
-export const getExercise = () => {
-  return axios.post(``);
 };
