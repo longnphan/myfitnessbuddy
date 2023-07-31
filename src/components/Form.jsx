ce945c56deb41b9605e2f7aa8b216ddd2ee9d55d
Long Phan <longnphan@outlook.com>
2023-07-31 14:50:32 -0600

    add calorie calculation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,15 +3,30 @@ import { useDispatch } from "react-redux";
 import { updateProfile } from "../store/userSlice";
 
 function Form() {
+  const [isComplete, setIsComplete] = useState(false);
   const [inputs, setInputs] = useState({
     name: "",
     age: "",
     height: "",
     weight: "",
+    calories: "",
     gender: "male",
   });
+  const [profile, setProfile] = useState([]);
 
   const dispatch = useDispatch();
+
+  const calcDailyCals = bio => {
+    if (bio.gender === "male") {
+      let maleResult = 15 * bio.weight;
+      return maleResult;
+    }
+
+    if (bio.gender === "female") {
+      let femaleResult = 12 * bio.weight;
+      return femaleResult;
+    }
+  };
 
   const handleOnChange = e => {
     const name = e.target.id;
@@ -22,17 +37,20 @@ function Form() {
 
   const handleSubmit = e => {
     e.preventDefault();
+    setInputs(prev => ({ ...prev, calories: calcDailyCals(inputs) }));
     dispatch(updateProfile(inputs));
+    setIsComplete(!isComplete);
   };
 
   return (
     <form className="mx-auto form-control w-full max-w-xs py-7">
-      <h1 className="text-3xl mx-auto mb-2">Create Profile</h1>
+      <h1 className="text-3xl mx-auto mb-2">
+        {isComplete ? "Edit Profile" : "Create Profile"}
+      </h1>
       <label className="label" htmlFor="name">
         <span className="label-text">What is your first name?</span>
       </label>
       <input
-        required
         id="name"
         type="text"
         placeholder="First name"
@@ -45,7 +63,6 @@ function Form() {
         <span className="label-text">What is your age?</span>
       </label>
       <input
-        required
         id="age"
         type="number"
         placeholder="Age"
@@ -58,7 +75,6 @@ function Form() {
         <span className="label-text">What is your height?</span>
       </label>
       <input
-        required
         id="height"
         type="number"
         placeholder="Height in inches"
@@ -71,7 +87,6 @@ function Form() {
         <span className="label-text">What is your weight?</span>
       </label>
       <input
-        required
         id="weight"
         type="number"
         placeholder="Weight in pounds"
@@ -97,7 +112,7 @@ function Form() {
         className="btn btn-primary w-1/3 mt-7 mx-auto"
         onClick={handleSubmit}
       >
-        Submit
+        {isComplete ? "Edit" : "Submit"}
       </button>
     </form>
   );
