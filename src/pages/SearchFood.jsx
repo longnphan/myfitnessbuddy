91979111a77432490a8b688cd53e6a281d824681
Long Phan <longnphan@outlook.com>
2023-07-31 19:04:22 -0600

    more styling refactor

--- src/pages/SearchFood.jsx
+++ src/pages/SearchFood.jsx
@@ -18,25 +18,27 @@ function SearchFood() {
 
   return (
     <div>
-      <form
-        className="mx-auto form-control w-full max-w-xs py-7"
-        onSubmit={handleSubmit}
-      >
-        <label className="label" htmlFor="search">
-          <span className="label-text">Search Food</span>
-        </label>
+      <div className="card w-3/5 mx-auto rounded bg-primary mt-6">
+        <div className="card-body items-center text-center">
+          <form
+            className="mx-auto form-control w-full max-w-xs py-7"
+            onSubmit={handleSubmit}
+          >
+            <input
+              id="search"
+              type="text"
+              placeholder="Search for food item"
+              className="input input-bordered bg-opacity-100 input-info bg-base-100 w-full max-w-xs"
+              value={searchInput}
+              onChange={e => setSearchInput(e.target.value)}
+            />
 
-        <input
-          id="search"
-          type="text"
-          placeholder="Search for food item"
-          className="input input-bordered input-info w-full max-w-xs"
-          value={searchInput}
-          onChange={e => setSearchInput(e.target.value)}
-        />
-
-        <button className="btn btn-primary w-1/3 mt-7 mx-auto">Search</button>
-      </form>
+            <button className="btn btn-neutral hover:bg-accent w-1/3 mt-7 mx-auto">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
 
       {renderFoodItems}
     </div>
