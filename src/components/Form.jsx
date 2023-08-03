3eb3b0b7b01375b8c8d6a6f1f08bc6a1120905a2
Long Phan <longnphan@outlook.com>
2023-08-03 08:45:14 -0600

    update styling on Form and SearchFoodCard

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,64 +23,59 @@ function Form() {
   };
 
   return (
-    <div className="mx-auto card w-3/5 bg-neutral text-neutral-content mt-6">
+    <div className="mx-auto card w-3/5 bg-primary text-neutral-content mt-6">
       <div className="card-body items-center text-center">
         <form className="mx-auto form-control w-full max-w-xs py-7">
-          <h1 className="text-3xl mx-auto mb-2">Profile</h1>
-          <label className="label" htmlFor="name">
-            <span className="label-text">What is your first name?</span>
-          </label>
-          <input
-            id="name"
-            type="text"
-            placeholder="First name"
-            className="input input-bordered input-info w-full max-w-xs"
-            value={inputs.name}
-            onChange={handleOnChange}
-          />
+          <h1 className="text-3xl mx-auto mb-2 text-neutral">Profile</h1>
 
           <label className="label" htmlFor="age">
-            <span className="label-text">What is your age?</span>
+            <span className="label-text text-neutral">What is your age?</span>
           </label>
           <input
             id="age"
             type="number"
             placeholder="Age"
-            className="input input-bordered input-info w-full max-w-xs"
+            className="input input-bordered text-black bg-sky-100 placeholder-gray border-black w-full max-w-xs"
             value={inputs.age}
             onChange={handleOnChange}
           />
 
           <label className="label" htmlFor="height">
-            <span className="label-text">What is your height?</span>
+            <span className="label-text text-neutral">
+              What is your height?
+            </span>
           </label>
           <input
             id="height"
             type="number"
             placeholder="Height in inches"
-            className="input input-bordered input-info w-full max-w-xs"
+            className="input input-bordered text-black bg-sky-100 placeholder-gray border-black w-full max-w-xs"
             value={inputs.height}
             onChange={handleOnChange}
           />
 
           <label className="label" htmlFor="weight">
-            <span className="label-text">What is your weight?</span>
+            <span className="label-text text-neutral">
+              What is your weight?
+            </span>
           </label>
           <input
             id="weight"
             type="number"
             placeholder="Weight in pounds"
-            className="input input-bordered input-info w-full max-w-xs"
+            className="input input-bordered text-black bg-sky-100 placeholder-gray border-black w-full max-w-xs"
             value={inputs.weight}
             onChange={handleOnChange}
           />
 
           <label className="label" htmlFor="gender">
-            <span className="label-text">What is your gender?</span>
+            <span className="label-text text-neutral">
+              What is your gender?
+            </span>
           </label>
           <select
             id="gender"
-            className="input input-info"
+            className="input input-bordered border-black bg-sky-100 text-black"
             value={inputs.gender}
             onChange={handleOnChange}
           >
@@ -89,7 +84,7 @@ function Form() {
           </select>
 
           <button
-            className="btn btn-primary w-1/3 mt-7 mx-auto"
+            className="btn btn-neutral hover:bg-accent w-1/3 mt-7 mx-auto"
             onClick={handleSubmit}
           >
             Submit
